2bd444c1aaa3e915c72c455aac0a105698c6c64d
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-02-15 16:53:04 +0100

    cors settings

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const nodemailer = require("nodemailer")
 
 const app = express()
 app.use(formidable())
-app.use(cors())
+// app.use(cors())
+
+const corsOptions = {
+	origin: "*",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
 	res.send("Server is up!")
